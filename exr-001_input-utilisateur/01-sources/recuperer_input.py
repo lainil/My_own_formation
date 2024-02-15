9eddf542b16fa927e17b30f4c7cbe4d23eebf77a
lainil <102599841+lainil@users.noreply.github.com>
2024-02-15 22:58:36 +0100

    input exercice

--- exr-001_input-utilisateur/01-sources/recuperer_input.py
+++ exr-001_input-utilisateur/01-sources/recuperer_input.py
@@ -1,2 +1,9 @@
 # Demander le prénom, la ville de naissance et l'âge de la personne qui utilise le script
+prenom=input('prénom:')
+ville_naissance=input('ville de naissance:')
+age=input('age:')
+
 # Afficher ces trois informations
+print('Tu t\'appelles, '+prenom+" tu es né(e) à "+ville_naissance+" et tu as "+age, end=".")
+
+"bonjour".isdigit
