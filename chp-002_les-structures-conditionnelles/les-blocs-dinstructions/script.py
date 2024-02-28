c98c7d8fe09cc9f73cbc7e746e86aa87bf8d1945
lainil <102599841+lainil@users.noreply.github.com>
2024-02-28 07:01:01 +0100

    2 files added : opticis & structure conditionnelles
    2 files added : opticis & structure conditionnelles

--- chp-002_les-structures-conditionnelles/les-blocs-dinstructions/script.py
+++ chp-002_les-structures-conditionnelles/les-blocs-dinstructions/script.py
@@ -1,9 +1,15 @@
-age = 20
-langage == "Python"
+##age = 20
+##langage == "Python"
 
-if age >= 18:
-    print("Vous êtes majeur !")
-    if langage == "Python":
-        print("Vous pouvez rentrer")
+####if age >= 18:
+  ##  print("Vous êtes majeur !")
+  ##  if langage == "Python":
+  ##      print("Vous pouvez rentrer")
 
-print("Le script est terminé")
+##print("Le script est terminé")##
+
+toto=float(input("quel age a toto?"))
+if toto>=18:
+    print("toto est grand!")
+else:
+    print("c'est de la merde")
